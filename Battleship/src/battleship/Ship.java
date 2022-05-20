fe9aa56dc78bdc30732e450f4dcaba5356449d83
natalia.ewa.bielecka@gmail.com <93135239+neb-dev-il@users.noreply.github.com>
2022-05-20 17:56:00 +0200

    Update Ship.java

--- Battleship/src/battleship/Ship.java
+++ Battleship/src/battleship/Ship.java
@@ -1,4 +1,37 @@
 package battleship;
 
-public enum Ship {
+enum Ship {
+    AIRCRAFT_CARRIER_SIZE("Aircraft Carrier", 5),
+    BATTLESHIP_SIZE("Battleship", 4),
+    SUBMARINE_SIZE("Submarine", 3),
+    CRUISER_SIZE("Cruiser", 3),
+    DESTROYER_SIZE("Destroyer", 2);
+
+    private final String NAME;
+    private final int LENGTH;
+
+    /**
+     * This is the Ship constructor
+     */
+
+    Ship(String name, int length) {
+        this.NAME = name;
+        this.LENGTH = length;
+    }
+
+    /**
+     * This is the LENGTH getter
+     */
+
+    int getLENGTH() {
+        return LENGTH;
+    }
+
+    /**
+     * This is the NAME getter
+     */
+
+    String getNAME() {
+        return NAME;
+    }
 }
