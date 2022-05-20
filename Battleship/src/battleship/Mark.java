671ecaaf49067847412d1366518a02508f4337cd
natalia.ewa.bielecka@gmail.com <93135239+neb-dev-il@users.noreply.github.com>
2022-05-20 17:40:25 +0200

    Update Mark.java

--- Battleship/src/battleship/Mark.java
+++ Battleship/src/battleship/Mark.java
@@ -1,4 +1,26 @@
 package battleship;
 
-public enum Mark {
+enum Mark {
+    FOG_OF_WAR('~'),
+    SHIP('O'),
+    HIT('X'),
+    MISS('M');
+
+    private final char MARK;
+
+    /**
+     * This is the Mark enumeration constructor
+     */
+
+    Mark(char mark) {
+        this.MARK = mark;
+    }
+
+    /**
+     * This is the mark getter
+     */
+
+    char getMARK() {
+        return MARK;
+    }
 }
