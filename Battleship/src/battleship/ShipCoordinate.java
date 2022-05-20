d710af3590bbab59b8af23a043950a0796a95b4a
natalia.ewa.bielecka@gmail.com <93135239+neb-dev-il@users.noreply.github.com>
2022-05-20 17:59:44 +0200

    Update ShipCoordinate.java

--- Battleship/src/battleship/ShipCoordinate.java
+++ Battleship/src/battleship/ShipCoordinate.java
@@ -1,4 +1,42 @@
 package battleship;
 
-public class ShipCoordinate {
+class ShipCoordinate extends ShipPosition {
+
+    private static String[] arrayOfCoordinates;
+
+    /**
+     * This is arrayOfCoordinates getter
+     */
+
+    static String[] getArrayOfCoordinates() {
+        return arrayOfCoordinates;
+    }
+
+    /**
+     * This is arrayOfCoordinates setter
+     */
+
+    static void processUserInputToArrayOfCoordinates(String userInput) {
+        ShipCoordinate.arrayOfCoordinates = userInput.split("\\s+");
+    }
+
+    @Override
+    int getColumn() {
+        return super.getColumn();
+    }
+
+    @Override
+    void setColumn(String input) {
+        super.setColumn(input);
+    }
+
+    @Override
+    int getRow() {
+        return super.getRow();
+    }
+
+    @Override
+    void setRow(String input) {
+        super.setRow(input);
+    }
 }
