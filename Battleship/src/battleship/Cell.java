a5476fe4784802a9b0326e1422f320071eba0165
natalia.ewa.bielecka@gmail.com <93135239+neb-dev-il@users.noreply.github.com>
2022-05-20 17:18:30 +0200

    Update Cell.java

--- Battleship/src/battleship/Cell.java
+++ Battleship/src/battleship/Cell.java
@@ -1,4 +1,41 @@
 package battleship;
 
-public class Cell {
+abstract class Cell {
+
+    private int row;
+    private int column;
+
+    /**
+     * This is the column getter
+     */
+
+    int getColumn() {
+        return column;
+    }
+
+    /**
+     * This is the column setter
+     */
+
+    void setColumn(String input) {
+        this.column = Integer.parseInt(input.substring(1)) - 1;
+    }
+
+    /**
+     * This is the row getter
+     */
+
+    int getRow() {
+        return row;
+    }
+
+    /**
+     * This is the row setter
+     */
+
+    void setRow(String input) {
+        this.row = (char) (input.charAt(0) - 65);
+    }
+
+    abstract boolean isMarkNearCell(int row, int column, char symbol, char[][] battlefield);
 }
